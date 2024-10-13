c10fcc4173fff292c3c0855f73594800674661fa
Eduard Drozdov <ekdrozdov@gmail.com>
2024-10-13 16:56:19 +0700

    scaffold game screen

---
+++ src/renderer/pixi/ui/GameScreen.ts
@@ -0,0 +1,12 @@
+import {} from "@pixi/ui";
+import { AppScreen } from "./AppScreen";
+
+/**
+ * Game screen.
+ * To be used to show all the game play and UI.
+ */
+export class GameScreen extends AppScreen {
+	constructor() {
+		super("GameScreen");
+	}
+}
